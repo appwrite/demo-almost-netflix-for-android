dc30ee9a555ca899ef025721a29d4be3e4f50a1e
Logan Sims <logan.sims94@gmail.com>
2022-10-26 18:06:58 -0700

    changes Account alias from User to AccountModel

--- app/src/main/java/io/appwrite/almostnetflix/splash/SplashViewModel.kt
+++ app/src/main/java/io/appwrite/almostnetflix/splash/SplashViewModel.kt
@@ -6,14 +6,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.appwrite.almostnetflix.core.Configuration
 import io.appwrite.exceptions.AppwriteException
-import io.appwrite.models.Account as User
+import io.appwrite.models.Account as AccountModel
 import io.appwrite.services.Account
 import kotlinx.coroutines.launch
 
 class SplashViewModel : ViewModel() {
 
-    private val _authUser = MutableLiveData<User?>()
-    val authUser: LiveData<User?> = _authUser
+    private val _authUser = MutableLiveData<AccountModel?>()
+    val authUser: LiveData<AccountModel?> = _authUser
 
     init {
         viewModelScope.launch {
